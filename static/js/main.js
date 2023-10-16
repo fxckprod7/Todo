a516a82b0561326e1567b48a8c3485e7b4dedb51
1gorekkk41 <98848448+Igorekkk41@users.noreply.github.com>
2023-10-16 18:23:51 +0300

    alpha concept of animation
    alpha concept of animation

--- static/js/main.js
+++ static/js/main.js
@@ -10,3 +10,43 @@ if (iconMenu){
         menuBody.classList.toggle('_active');
     });
 }
+
+/* -------------------------------------------------------------------------- */
+
+
+function showImage(side) {
+    if (window.innerWidth > 768) {
+        if (side === 'left') {
+            let leftImage = document.getElementById('left-image');
+            leftImage.style.display = 'block';
+            leftImage.style.animation = "slideInLeft 1s forwards";
+        } else if (side === 'right') {
+            let rightImage = document.getElementById('right-image');
+            rightImage.style.display = 'block';
+            rightImage.style.animation = "slideInRight 1s forwards";
+        }
+    }
+}
+
+function hideImage(side) {
+    if (window.innerWidth > 768) {
+        if (side === 'left') {
+            let leftImage = document.getElementById('left-image');
+            leftImage.style.animation = "slideOutLeft 1s forwards";
+            setTimeout(function() {
+                leftImage.style.animation = '';
+            }, endless);
+        } 
+        else if (side === 'right') {
+            let rightImage = document.getElementById('right-image');
+            rightImage.style.animation = "slideOutRight 1s forwards";
+            setTimeout(function() {
+                rightImage.style.animation = '';
+            }, endless);
+        }
+    }
+}
+/* -------------------------------------------------------------------------- */
+
+
+
