ac30400396632308591139c03d99a00d75ccff39
Maksym Sharai <dev.maksim.sharay@gmail.com>
2023-10-20 14:48:27 +0100

    Try to fix 500

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def page_not_found(error):
 @app.route('/')
 def index():
     # shoping_tasks = db.get_shoping()
-    shoping_tasks = []
+    shoping_tasks = ""
     return render_template("index.html", shoping_tasks=shoping_tasks)
 
 
